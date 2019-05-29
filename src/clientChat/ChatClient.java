63b9cd3a621e3232521b49f7d304d213246f0173
Tomasz Smolen <tomaszsmolen97@gmail.com>
2019-05-29 20:40:13 +0200

    project rework

--- src/clientChat/ChatClient.java
+++ src/clientChat/ChatClient.java
@@ -2,8 +2,10 @@ package clientChat;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Date;
 
 import javax.xml.soap.Text;
 
@@ -18,13 +20,17 @@ public class ChatClient {
     private static DataInputStream inputData;
     private static DataOutputStream outputData;
 
-    public static void createClient(String serverAddress, String serverPort){
+    public static void createClient(String serverAddress, String serverPort) throws IOException {
         TextArea clientChatWindow = (TextArea) Main.getMainScene().lookup("#clientChatWindow");
         TextField messageSendInput = (TextField) Main.getMainScene().lookup("#clientSendMessageField");
         TextField clientNickname = (TextField) Main.getMainScene().lookup("#nickClientLabel");
         Button sendMessage = (Button) Main.getMainScene().lookup("#btClientSendMessage");
+        Button connect = (Button) Main.getMainScene().lookup("btConnect");
         clientChatWindow.setEditable(false);
         clientNickname.setEditable(false);
+        sendMessage.setDisable(false);
+        messageSendInput.setDisable(false);
+
         new Thread(() -> {
             try {
                 String recievedMessage = "";
@@ -32,10 +38,17 @@ public class ChatClient {
                 inputData = new DataInputStream(newClient.getInputStream());
                 outputData = new DataOutputStream(newClient.getOutputStream());
 
+                if(clientNickname.getText().equals("")){
+                    outputData.writeUTF("Guest");
+                }
+                else{
+                    outputData.writeUTF(clientNickname.getText());
+                }
                 while(!recievedMessage.equals("logout")){
                     recievedMessage = inputData.readUTF();
                     clientChatWindow.appendText(recievedMessage + "\n");
                 }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -44,12 +57,13 @@ public class ChatClient {
         sendMessage.setOnAction(event -> {
             try {
                 String messageToSend;
+
                 String clientNick = clientNickname.getText();
                 if(clientNick.equals("")){
                     clientNick = "Guest";
                 }
                 messageToSend = messageSendInput.getText();
-                clientChatWindow.appendText(clientNick + ": " + messageToSend + "\n");
+
                 outputData.writeUTF(clientNick + ": " + messageToSend);
                 messageSendInput.setText("");
             } catch (IOException e) {
--- src/clientChat/controller/ClientGuiController.java
+++ src/clientChat/controller/ClientGuiController.java
@@ -1,11 +1,28 @@
 package clientChat.controller;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import clientChat.ChatClient;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.InnerShadow;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
-public class ClientGuiController {
+public class ClientGuiController implements Initializable {
+    private Color color = Color.WHITE;
     @FXML
     private TextField clientPortField;
 
@@ -14,8 +31,59 @@ public class ClientGuiController {
 
     @FXML
     private Button btConnect;
+
     @FXML
-    public void connectToServer(){
+    private ColorPicker labelColorPicker;
+
+    @FXML
+    private ColorPicker fontColorPicker;
+
+    @FXML
+    private ColorPicker bgColorPicker;
+
+    @FXML
+    private ColorPicker textColorPicker;
+
+    @FXML
+    private TextArea clientChatWindow;
+
+    @FXML
+    private TextField clientSendMessageField;
+
+    @FXML
+    private TextField nickClientLabel;
+
+    @FXML
+    private AnchorPane mainAnchor;
+
+    @FXML
+    private Label portLabel;
+
+    @FXML
+    private Label addLabel;
+
+    @FXML
+    private Label nickLabel;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private Label label2;
+
+    @FXML
+    private Label label3;
+
+    @FXML
+    private Label label4;
+
+    @FXML
+    private ScrollPane areaScrollPane;
+
+    @FXML
+    private AnchorPane areaAnchor;
+    @FXML
+    public void connectToServer() throws IOException {
     String connectionPort = clientPortField.getText();
     String connectionAddress = clientAddressField.getText();
 
@@ -23,5 +91,59 @@ public class ClientGuiController {
     clientPortField.setEditable(false);
     clientAddressField.setEditable(false);
     btConnect.setDisable(true);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        labelColorPicker.setOnAction(event -> {
+            color = labelColorPicker.getValue();
+            InnerShadow innerShadow = new InnerShadow();
+            innerShadow.setOffsetX(0);
+            innerShadow.setOffsetY(0);
+            innerShadow.setRadius(2);
+            innerShadow.setColor(Color.web("0x000000"));
+
+            clientPortField.setBackground(new Background(new BackgroundFill(color, new CornerRadii(3), Insets.EMPTY)));
+            clientPortField.setEffect(innerShadow);
+
+            clientAddressField.setBackground(new Background(new BackgroundFill(color, new CornerRadii(3), Insets.EMPTY)));
+            clientAddressField.setEffect(innerShadow);
+
+            nickClientLabel.setBackground(new Background(new BackgroundFill(color, new CornerRadii(3), Insets.EMPTY)));
+            nickClientLabel.setEffect(innerShadow);
+
+            clientSendMessageField.setBackground(new Background(new BackgroundFill(color, new CornerRadii(3), Insets.EMPTY)));
+            clientSendMessageField.setEffect(innerShadow);
+
+            clientChatWindow.setEffect(innerShadow);
+
+            areaAnchor.setStyle("-fx-base: "+ format(color) +";");
+            areaScrollPane.setStyle("-fx-base: "+ format(color) +";");
+        });
+        fontColorPicker.setOnAction(event -> {
+            nickClientLabel.setStyle("-fx-text-fill: " + format(fontColorPicker.getValue()) + ";");
+            clientPortField.setStyle("-fx-text-fill: " + format(fontColorPicker.getValue()) + ";");
+            clientAddressField.setStyle("-fx-text-fill: " + format(fontColorPicker.getValue()) + ";");
+            clientSendMessageField.setStyle("-fx-text-fill: " + format(fontColorPicker.getValue()) + ";");
+            clientChatWindow.setStyle("-fx-text-fill: " + format(fontColorPicker.getValue()) + ";");
+        });
+        bgColorPicker.setOnAction(event -> mainAnchor.setStyle("-fx-background-color: "+ format(bgColorPicker.getValue()) +";"));
+
+        textColorPicker.setOnAction(event -> {
+            String url = "-fx-text-fill: " + format(textColorPicker.getValue()) + ";";
+            portLabel.setStyle(url);
+            addLabel.setStyle(url);
+            nickLabel.setStyle(url);
+            label1.setStyle(url);
+            label2.setStyle(url);
+            label3.setStyle(url);
+            label4.setStyle(url);
+        });
+    }
+    private String format(Color c) {
+        int r = (int) (255 * c.getRed());
+        int g = (int) (255 * c.getGreen());
+        int b = (int) (255 * c.getBlue());
+        return String.format("#%02x%02x%02x", r, g, b);
     }
 }
